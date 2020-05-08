30807af603f5c15e48cbcff389d720b31a208754
IndominusByte <nyomanpradipta120@gmail.com>
2020-05-08 12:00:55 +0800

    deploy to heroku

--- services/serve.py
+++ services/serve.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from services.config import Development
+from services.config import Production
 
 app = Flask(__name__)
-app.config.from_object(Development)
+app.config.from_object(Production)
 
 from services.resources.sion.route import sions
 app.register_blueprint(sions)
